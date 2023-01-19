46ccd4a2381dd615f78433e106769979e577a704
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-01-19 23:24:49 +0200

    commit-1

---
+++ src/fetchPhotos.js
@@ -0,0 +1,22 @@
+export { fetchPhotos };
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '32968388-b31919f1c04eebb6402c46dda';
+
+const options = {
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+};
+
+async function fetchPhotos(searchQuery, page = 1) {
+  const response = await axios.get(
+    `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation =horizontal&safesearch =true&per_page=40&page=${page}`
+  );
+  return response.data;
+}
+// fetchPhotos()
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,98 @@
+import { fetchPhotos } from './fetchPhotos';
+
 import axios from 'axios';
-import Notiflix from 'notiflix';
+import Notiflix, { Loading } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+
+let page = 1;
+
+formEl.addEventListener('submit', onSearch);
+btnLoadMore.addEventListener('click', onLoadMore);
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onSearch(e) {
+  e.preventDefault();
+  galleryEl.innerHTML = '';
+
+  searchQuery = formEl.children[0].value.trim();
+
+  if (searchQuery.length === 0) {
+    return Notiflix.Notify.failure('Please, enter a query.');
+  }
+
+  fetchPhotos(searchQuery)
+    .then(data => {
+      console.log(data);
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        galleryMarkup(data.hits);
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        btnLoadMore.hidden = false;
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+function galleryMarkup(arr) {
+  const markup = arr
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+  <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy"/></a>
+  <div class="info">
+    <p class="info-item">
+    <b>Likes</b> <span class="info-item-api"> ${likes} </span>
+</p>
+            <p class="info-item">
+                <b>Views</b> <span class="info-item-api">${views}</span>  
+            </p>
+            <p class="info-item">
+                <b>Comments</b> <span class="info-item-api">${comments}</span>  
+            </p>
+            <p class="info-item">
+                <b>Downloads</b> <span class="info-item-api">${downloads}</span> 
+            </p>
+  </div>
+</div>`
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  const gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+  });
+}
+
+function onLoadMore() {
+  page += 1;
+
+  fetchPhotos(page).then(data => {
+    galleryMarkup(data.hits);
+    if (data.hits.total === data.hits.totalHits) {
+      btnLoadMore.hidden = true;
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  });
+}
