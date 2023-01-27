70a18de5e65d6a7b0e466cd40fe46ea9d1828f84
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-01-27 18:18:01 +0200

    commit

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '32968388-b31919f1c04eebb6402c46dda';
 
-async function fetchPhotos(searchQuery, page = 1) {
+async function fetchPhotos(searchQuery, page) {
   const response = await axios.get(
     `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation =horizontal&safesearch =true&page=${page}&per_page=40`
   );
